5bf4ef9f93100521f061093dc04d7ee28e803f7f
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-01-10 02:30:56 +0500

    Complete Project 1.1: Simple Calculator

---
+++ Phase1-HTMLForms/Project1.1-Calculator/SimpleCalculator/SimpleCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+            switch(operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            ViewBag.Result = result;
+            return View(); 
+        }
+    }
+}
