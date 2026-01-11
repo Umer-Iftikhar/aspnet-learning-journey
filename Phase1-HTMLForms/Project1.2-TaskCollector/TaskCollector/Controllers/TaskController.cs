d87f20d91502031b4d3e20e36f90020b3947e312
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-01-12 02:39:35 +0500

    Complete Project 1.2: Task Collector

---
+++ Phase1-HTMLForms/Project1.2-TaskCollector/TaskCollector/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCollector.Controllers
+{
+    public class TaskController : Controller
+    {
+        static List<string> tasks = new List<string>();
+        public IActionResult Index()
+        {
+            ViewBag.Tasks = tasks;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(string task)
+        {
+            tasks.Add(task);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult ClearAll()
+        {
+            tasks.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
