a265b6a69c2effcd63b784f36134dd8eaa62b8f0
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-01-12 20:55:56 +0500

    Phase 1.3: Implemented Contact Form with Model Binding and PRG pattern

---
+++ Phase1-HTMLForms/Project1.3-ContactForm/ContactForm/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using ContactForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactForm.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Submit(ContactSubmission model)
+        {
+            //TempData["Submission"] = model;
+            TempData["UserName"] = model.Name;
+            return RedirectToAction("ThankYou");
+        }
+        [HttpGet]
+        public IActionResult ThankYou()
+        {
+            var name = TempData["UserName"];
+            ViewBag.Name = name;
+            return View(); 
+        }
+    }
+}
