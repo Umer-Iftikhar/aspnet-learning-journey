ec4b2b05e5dd9c8bb418093e5116060b11f6252e
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-01-21 01:48:38 +0500

    Recreated 2.3

--- Phase 2-Razor Syntax/Project 2.3-LoginSystem/LoginSystem/Controllers/HomeController.cs
+++ Phase 2-Razor Syntax/Project 2.3-LoginSystem/LoginSystem/Controllers/HomeController.cs
@@ -1,24 +1,25 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using LoginSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace LoginSystem.Controllers;
-
-public class HomeController : Controller
+namespace LoginSystem.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- Phase 2-Razor Syntax/Project 2.3-LoginSystem/LoginSystem/Models/ErrorViewModel.cs
+++ Phase 2-Razor Syntax/Project 2.3-LoginSystem/LoginSystem/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace LoginSystem.Models;
-
-public class ErrorViewModel
+namespace LoginSystem.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
