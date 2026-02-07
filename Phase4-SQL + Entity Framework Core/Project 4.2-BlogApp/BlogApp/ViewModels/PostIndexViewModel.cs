8ea2217ee9880702d93850ec2c04580590383302
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-02-07 21:17:36 +0500

    Add ViewModels for Blog - PostListViewModel, PostIndexViewModel, PostFormViewModel

---
+++ Phase4-SQL + Entity Framework Core/Project 4.2-BlogApp/BlogApp/ViewModels/PostIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.ViewModels
+{
+    public class PostIndexViewModel
+    {
+        public List<PostListViewModel> Posts { get; set; } = new List<PostListViewModel>();
+        public string SearchQuery { get; set; } = string.Empty;
+        public string SortOrder { get; set; } = string.Empty;
+    }
+}
