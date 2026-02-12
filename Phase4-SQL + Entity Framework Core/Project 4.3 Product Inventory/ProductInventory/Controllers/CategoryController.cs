40a4a5b5aa86dee1330cca784792aa47b796d6cd
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-02-12 20:47:35 +0500

    Phase 4.3: Add Category Create action and view

---
+++ Phase4-SQL + Entity Framework Core/Project 4.3 Product Inventory/ProductInventory/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductInventory.Data;
+using ProductInventory.Models;
+
+namespace ProductInventory.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCategories = await _context.Categories.ToListAsync();
+            return View(allCategories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+           
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
+ 
