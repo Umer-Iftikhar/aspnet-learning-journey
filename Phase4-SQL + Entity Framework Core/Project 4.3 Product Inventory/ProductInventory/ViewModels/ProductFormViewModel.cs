5edb80452be20102b4a28538946039bd48b460aa
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-02-15 17:19:43 +0500

    Created VIewModel

---
+++ Phase4-SQL + Entity Framework Core/Project 4.3 Product Inventory/ProductInventory/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProductInventory.Models;
+
+namespace ProductInventory.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public Product product;
+        public List<Category> Categories { get; set; } = new();
+    }
+}
