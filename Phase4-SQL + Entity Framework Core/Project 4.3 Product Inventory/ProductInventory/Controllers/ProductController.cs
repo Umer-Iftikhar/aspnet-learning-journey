7c6d3eb86703fcf9c12642f18d4c97a50158c6b8
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-02-15 17:10:47 +0500

    Implemented product Index method in controller and created Product/ Index.cshtml

---
+++ Phase4-SQL + Entity Framework Core/Project 4.3 Product Inventory/ProductInventory/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductInventory.Data;
+
+namespace ProductInventory.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _context.Products.Include(p => p.Category).ToListAsync();
+            return View(allProducts);
+        }
+    }
+}
