42da67031252895a61c93a496e601cabe70d4b35
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-03-07 21:42:09 +0500

    Created Controller

---
+++ Phase6-Authentication + Authorization/Project 6.1-AuthSystem/AuthSystem/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using AuthSystem.Models;
+using AuthSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInmanager)
+        {
+            _userManager = userManager;
+            _signInManager = signInmanager;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new ApplicationUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) 
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
+
+
