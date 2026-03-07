63de102d8b58fa7bd6c146284cb4dadb5d13bea1
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-03-07 23:15:31 +0500

    Completed login and logout

--- Phase6-Authentication + Authorization/Project 6.1-AuthSystem/AuthSystem/Controllers/AccountController.cs
+++ Phase6-Authentication + Authorization/Project 6.1-AuthSystem/AuthSystem/Controllers/AccountController.cs
@@ -14,7 +14,7 @@ namespace AuthSystem.Controllers
             _userManager = userManager;
             _signInManager = signInmanager;
         }
-        
+
         public IActionResult Index()
         {
             return View();
@@ -38,7 +38,7 @@ namespace AuthSystem.Controllers
             };
             var result = await _userManager.CreateAsync(user, model.Password);
 
-            if (result.Succeeded) 
+            if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction("Index", "Home");
@@ -50,8 +50,36 @@ namespace AuthSystem.Controllers
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-
             return View(model);
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
