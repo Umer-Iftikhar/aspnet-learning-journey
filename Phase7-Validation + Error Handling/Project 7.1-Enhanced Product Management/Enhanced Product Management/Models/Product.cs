6cffe7b8e4e10481d21807320c6895e3d5b72775
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-09 19:58:46 +0500

    Proved validations work manually

--- Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Models/Product.cs
+++ Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Models/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace Enhanced_Product_Management.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Enhanced_Product_Management.Validations;
+
+namespace Enhanced_Product_Management.Models
 {
+    [PriceRequiredIfInStock]
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public decimal Stock { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
 
-    }
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, 10000, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "Stock must be within the limit (0-10000).")]
+        public int Stock { get; set; }
+    }   
 }
--- Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Validations/PriceRequiredIfInStockAttribute.cs
+++ Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Validations/PriceRequiredIfInStockAttribute.cs
@@ -4,12 +4,10 @@ namespace Enhanced_Product_Management.Validations
 {
     public class PriceRequiredIfInStockAttribute : ValidationAttribute
     {
-        public override ValidationResult IsValid(object value, ValidationContext context)
+        protected override ValidationResult IsValid(object? value, ValidationContext context)
         {
             var product = context.ObjectInstance as Product;
-            //ValidationResult ValidationReault = new ValidationResult("Price is required when the product is in stock.");
             
-
             if (product != null)
             {
                 if (product.Stock > 0 && product.Price <= 0)
@@ -17,9 +15,9 @@ namespace Enhanced_Product_Management.Validations
                     return  new ValidationResult("Price is required when the product is in stock.");
                 }
               
-                return   ValidationResult.Success;
-                
+                return   ValidationResult.Success!;
             }
+            return ValidationResult.Success!;
         }
     }
 }
