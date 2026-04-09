6f3d8416d9e2c03b4207c2a876510ec2ae8387a1
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-09 19:09:28 +0500

    Added the Views with validations

---
+++ Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Enhanced_Product_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enhanced_Product_Management.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Product());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+           return View(product);
+        }
+    }
+}
