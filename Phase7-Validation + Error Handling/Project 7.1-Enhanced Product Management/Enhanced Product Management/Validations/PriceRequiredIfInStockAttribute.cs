3dd67d7f319e1fa946c1e69fdb6d4eb0011ea1cd
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-09 18:54:19 +0500

    Created the Validation class for project 7.1

---
+++ Phase7-Validation + Error Handling/Project 7.1-Enhanced Product Management/Enhanced Product Management/Validations/PriceRequiredIfInStockAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Enhanced_Product_Management.Models;
+using System.ComponentModel.DataAnnotations;
+namespace Enhanced_Product_Management.Validations
+{
+    public class PriceRequiredIfInStockAttribute : ValidationAttribute
+    {
+        public override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var product = context.ObjectInstance as Product;
+            //ValidationResult ValidationReault = new ValidationResult("Price is required when the product is in stock.");
+            
+
+            if (product != null)
+            {
+                if (product.Stock > 0 && product.Price <= 0)
+                {
+                    return  new ValidationResult("Price is required when the product is in stock.");
+                }
+              
+                return   ValidationResult.Success;
+                
+            }
+        }
+    }
+}
